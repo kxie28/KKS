e29c865256ab8dda7d63294a9a403d491c6b1fa9
ksxie28 <kevinsxie28@gmail.com>
2022-12-13 16:07:31 -0500

    added content for header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>
-        Header
+    <div className="KKS__header section_padding" id="home">
+        <div className="KKS__header-content">
+          <h1 className="gradient__text">Let’s Build Something amazing with K&K Strategies</h1>
+          <p>Assisting with problems is our mantra. The possibilities are endless.</p>
+
+          <div className="KKS__header-content__input">
+            <input type="email" placeholder="Your Email Address"/>
+            <button type="button">Get Started</button>
+          </div>
+
+          <div className="KKS__header-content__people">
+            <img src={people} alt="people"/>
+            <p>1,600 people requested access in the last 24 hours</p>
+          </div>
+        </div>
+        <div className="KKS__header-image">
+          <img src={ai} alt="ai"/>
+        </div>
     </div>
   )
 }
