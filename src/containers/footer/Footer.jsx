07cdbeb6c4886839d162efddedf39cf8b64a716c
Kelly Xie <kellyjxie@gmail.com>
2022-12-13 22:24:38 -0500

    Add links to pages in Navbar and rearranged Header to act as a homepage

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,8 +3,14 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className="footer">
+      <footer class="py-5 bg-dark fixed-bottom">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; K&K Strategies
+          </p>
+        </div>
+      </footer>
     </div>
   )
 }
--- src/containers/whatKKS/WhatKKS.jsx
+++ src/containers/whatKKS/WhatKKS.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Navbar } from '../../components';
+
 import './whatKKS.css';
 
 const WhatKKS = () => {
   return (
-    <div>
-        WhatKKS
+    <div className="WhatKKS">
+      <div className="gradient__bg">
+          WhatKKS
+      </div>        
     </div>
   )
 }
