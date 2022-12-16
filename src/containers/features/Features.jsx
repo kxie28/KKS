f3c5e7389daf65d6a4506d55deba3ac365da59f8
ksxie28 <kevinsxie28@gmail.com>
2022-12-16 14:36:20 -0500

    adding to features page

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Feature } from '../../components'
+import React from 'react';
+import { Feature } from '../../components';
 import './features.css';
 
 const featuresData = [
@@ -30,7 +30,7 @@ const Features = () => {
       </div>
       <div className="KKS__features-container">
         {featuresData.map((item, index) => (
-          <Feature />
+          <Feature title={item.title} text={item.text} key={item.title + index}/>
         ))}
       </div>
     </div>
