0e07dade35bdc1fe71a1aad6426b27aca6041f3e
Kelly Xie <kellyjxie@gmail.com>
2023-01-05 23:01:07 -0500

    Adding some content to WhatKKS and minor Footer css styling

--- src/containers/whatKKS/WhatKKS.jsx
+++ src/containers/whatKKS/WhatKKS.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Navbar } from '../../components';
 
 import './whatKKS.css';
 
 const WhatKKS = () => {
   return (
-    <div className="WhatKKS">
-      <div className="gradient__bg">
-          WhatKKS
-      </div>        
+    <div className="KKS_whatKKS section__padding">
+      <div className="KKS__whatKKS-body">
+        <h1 className="gradient__text">We are committed to reaching your goals.</h1>
+        <p>
+          We are driven to help our local businesses grow. We tailor solutions based on your specific needs to streamline your business.
+        </p>
+        <h1 className="gradient__text">Who We Are</h1>
+        <p>
+          We are a brother-sister team with 10+ years of combined experience working on enterprise applications for large companies like Amazon and Salesforce. We are here to help you scale your applications as your business grows.
+        </p>
+      </div>   
     </div>
   )
 }
